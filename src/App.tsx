bc008d5acb0e0367211209ab35116e2f098c6ae4
LeafField <109466326+LeafField@users.noreply.github.com>
2024-04-30 20:02:13 +0900

    レスポンシブ対応しました。

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import styles from "./App.module.css";
 import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
 import Table from "./components/table/Table";
+import useBodyOverflowListener from "./hooks/useBodyOverflowListener";
+import MenuButton from "./components/menu-button/MenuButton";
 
 const App: FC = () => {
+  const { overflowY } = useBodyOverflowListener();
   return (
-    <div className={styles.body}>
+    <div className={styles.body} style={overflowY}>
       <Header />
       <div className={styles.container}>
         <Nav />
         <Table />
       </div>
+      <MenuButton />
     </div>
   );
 };
